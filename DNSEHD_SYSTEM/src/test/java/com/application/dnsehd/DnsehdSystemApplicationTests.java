2a728b8852dbdbe081d0b25322adfa2ef592087e
fusiontwo <yr0955@naver.com>
2024-01-10 01:37:40 +0900

    20240110

---
+++ DNSEHD_SYSTEM/src/test/java/com/application/dnsehd/DnsehdSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.dnsehd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnsehdSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
